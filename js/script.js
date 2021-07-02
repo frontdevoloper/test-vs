19fab40f5f25e5fe4321c1ba5cc4b2cf01fd47f4
frontdevoloper <frontdevop@gmail.com>
2021-07-02 12:37:46 +0200

    object user

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-console.log("hello;");
-console.log(25 + 25);
+"use strict";
+
+let user = {
+  userName: "Matt",
+  userAge: 16,
+  adress: "Port Dandre",
+  compane: "Schimmel - Heaney",
+  printData: (
+    name = "Rollin",
+    lastName = "Goldner",
+    secondName = "Hector Treutel"
+  ) => {
+    console.log(`Name: ${name}, LastName: ${lastName}`);
+  },
+};
+
+user.printData();
