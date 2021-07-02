3f040cbb2b2cd47a479b6284d58064bd04ffbba0
frontdevoloper <frontdevop@gmail.com>
2021-07-02 13:14:57 +0200

    Added user object

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const user = {
+  name: "Judah",
+  age: 22746.22222,
+  userArrCharName: [],
+  printData() {
+    this.userArrCharName = this.name.split("");
+    console.log(
+      `Name: ${this.name} and age: ${this.age} And by letters name: ${this.userArrCharName}`
+    );
+  },
+};
+
+user.printData();
