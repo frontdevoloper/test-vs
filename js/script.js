defdedc10180962f62ba1983b4cbc13644ca8fb6
frontdevoloper <frontdevop@gmail.com>
2021-07-02 12:39:05 +0200

    delete user

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
 "use strict";
-
-let user = {
-  userName: "Matt",
-  userAge: 16,
-  adress: "Port Dandre",
-  compane: "Schimmel - Heaney",
-  printData: (
-    name = "Rollin",
-    lastName = "Goldner",
-    secondName = "Hector Treutel"
-  ) => {
-    console.log(`Name: ${name}, LastName: ${lastName}`);
-  },
-};
-
-user.printData();
